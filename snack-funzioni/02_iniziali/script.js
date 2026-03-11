9fe91150c42361547952628d236d6056cef06806
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-11 16:37:54 +0100

    Complete 02 iniziali

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -2,12 +2,23 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+function onlyInitial (value) {
 
+    for (let i = 0; i < value.length; i++) {
+
+        let element = value[i];
+
+        for (let e = 0; e < 1; e++) {
+
+            let firstLetter = element[e];
+            console.log(firstLetter);
+
+        }
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+onlyInitial(names);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
