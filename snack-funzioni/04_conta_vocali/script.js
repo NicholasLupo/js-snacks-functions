9cf7b5eaf7a916740a50411bdc791cd78adb3fe5
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-11 17:05:51 +0100

    Complete 04 conta vocali

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -2,12 +2,37 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
+const onlyVocals = (text) => {
 
+    let vocals = ['a', 'e', 'i', 'o', 'u'];
+    let countVocals = '';
+    
+    for (let i = 0; i <= text.length; i++) {
+
+        let letter = text[i];
+
+        for (let p = 0; p < vocals.length; p++) {
+
+            let singleVocal = vocals[p];
+
+            if (letter == singleVocal) {
+
+                countVocals += letter;
+                console.log(letter);
+
+            }
+        }
+
+        if (i == text.length) {
+
+            console.log(countVocals.length);
+
+        }
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+onlyVocals(word);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
